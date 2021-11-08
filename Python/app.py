b6f68ad52e40144f1b1abef7d10c2ada39ca1328
Ben <benjamin.monge@gmail.com>
2021-11-08 10:59:00 -0600

    errors were popping up so i comment everythin out

--- Python/app.py
+++ Python/app.py
@@ -1,6 +1,6 @@
 #Libraries
-import qrcode
-import mysql.connector
+# import qrcode
+# import mysql.connector
 from flask import Flask, render_template
 
 #Run Flask
@@ -12,27 +12,27 @@ def home():
 if __name__ == "__main__":
     app.run(debug=True)
 
-#Python Connect to mysql Database
-cnx = mysql.connector.connect(user='root', password='Tdcgid2021!',
-                              host='127.0.0.1',
-                              database='qrcode')
-cursor = cnx.cursor()
-query = ("SELECT * FROM qrcode.ingredientslist")                             
-cursor.execute(query)
-for (Ingredient2) in cursor:
-  dataStored = Ingredient2
-cursor.close()
-cnx.close()
+# #Python Connect to mysql Database
+# cnx = mysql.connector.connect(user='root', password='Tdcgid2021!',
+#                               host='127.0.0.1',
+#                               database='qrcode')
+# cursor = cnx.cursor()
+# query = ("SELECT * FROM qrcode.ingredientslist")                             
+# cursor.execute(query)
+# for (Ingredient2) in cursor:
+#   dataStored = Ingredient2
+# cursor.close()
+# cnx.close()
 
-#Create QR Code
-website = "https://google.com"
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
-qr.add_data(website)
-qr.make(fit=True)
-img = qr.make_image(fill='black', back_color='white')
-img.save("Test.png")
+# #Create QR Code
+# website = "https://google.com"
+# qr = qrcode.QRCode(
+#     version=1,
+#     error_correction=qrcode.constants.ERROR_CORRECT_L,
+#     box_size=10,
+#     border=4,
+# )
+# qr.add_data(website)
+# qr.make(fit=True)
+# img = qr.make_image(fill='black', back_color='white')
+# img.save("Test.png")
